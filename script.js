d96830950839d1252c6e27f493eec634b6cf347f
sonali <sonalipriya902@gmail.com>
2024-03-29 01:02:25 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Smooth scroll functionality
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
+          e.preventDefault();
+  
+          const target = document.querySelector(this.getAttribute('href'));
+  
+          if (target) {
+              target.scrollIntoView({
+                  behavior: 'smooth'
+              });
+          }
+      });
+    });
+  });
+  function generate(){
+    alert("submitted");
+  }
